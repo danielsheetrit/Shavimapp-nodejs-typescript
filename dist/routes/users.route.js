77d4b3c22d8eb3b22911626871136fee21bba1d8
danielsheetrit <danielshitrit022@gmail.com>
2023-06-12 12:10:46 +0300

    render deploy #1

--- dist/routes/users.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const users_controller_1 = require("../controllers/users.controller");
-const userRouter = (0, express_1.Router)();
-userRouter.post('/register', users_controller_1.register).post('/login', users_controller_1.login);
-exports.default = userRouter;
-//# sourceMappingURL=users.route.js.map
--- dist/utils/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isEmpty = void 0;
-const isEmpty = (props) => {
-    return props.some((prop) => !prop || !prop.trim().length);
-};
-exports.isEmpty = isEmpty;
-//# sourceMappingURL=index.js.map
